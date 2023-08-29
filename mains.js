f8cb82d88f8753bbf25479b6687612d70df51c91
ThalesBergamim <139825263+ThalesBergamim@users.noreply.github.com>
2023-08-29 19:23:51 -0300

    Add files via upload

--- mains.js
+++ mains.js
@@ -6,14 +6,15 @@
     const campoB = parseFloat(document.getElementById('campoB').value);
     const mensagemSucesso = `Sucesso! Valor do campo B é de ${campoB} , maior que o valor do campo A.`
     const mensagemError = `Erro! Valor do campo A é de ${campoA}, maior que o valor do campo B.`
+    
 
     if (campoB > campoA) {
         const containerMensagemSucesso = document.querySelector('.sucess-message');
         containerMensagemSucesso.innerHTML = mensagemSucesso; 
         containerMensagemSucesso.style.display = 'block'; 
 
-        campoA.value = '';
-        campoB.value = '';
+        document.getElementById('campoA').value = '';
+        document.getElementById('campoB').value = '';
 
     } else {
         const containerMensagemSucesso = document.querySelector('.error-message');
